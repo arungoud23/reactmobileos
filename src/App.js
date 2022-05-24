28c96c1be994a5c1e12189c3ea4b4ffb8115572d
arungoud23 <100275592+arungoud23@users.noreply.github.com>
2022-05-24 19:14:44 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Answer from './component/Answer';
+
+
 
 function App() {
+
+  let op = ["Android","Blackberry","iphone","windowsphone"]
+  let manu = ["samsung","htc","micromax","Apple"]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {/* 1 st method */}
+      {/* <h2>Mobile Operating System</h2>
+      <Answer point="Android" />
+      <Answer point="Blackberry" />
+      <Answer point="iphone" />
+      <Answer point="windowsphone" />
+      <h2>Mobile Manufactureres</h2>
+      <Answer point="Samsung" />
+      <Answer point="htc" />
+      <Answer point="micromax" />
+      <Answer point="Apple" /> */}
+
+      {/* 2nd method */}
+       <h2>Mobile Operating System</h2>
+  {op.map((elem) => (
+    <Answer point={elem} />
+  ))}
+   <h2>Mobile Manufactureres</h2>
+
+  {manu.map(elem =>(
+    <Answer point={elem} />
+  ))}
+   
+
+ 
+
+     
     </div>
   );
 }
---
+++ src/component/Answer.jsx
@@ -0,0 +1,14 @@
+
+
+import React from 'react'
+
+const Answer = (props) => {
+  return <ul>
+      <li>{props.point}</li>
+      
+  </ul>
+  
+  
+}
+
+export default Answer
